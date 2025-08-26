d99cb02885e8726325db31e6c0abfbc14feac908
Kevin Gramajo <kevinalexisgramajo@gmail.com>
2025-08-25 23:35:14 -0300

    Save1

--- EjerciciosCondicionales.py
+++ EjerciciosCondicionales.py
@@ -1,2 +1,10 @@
-PlayerHeight=float(input("Ingresar Altura: "))
+PlayerHeight = float(input("Ingresar Altura: "))
 print(f"Tu altura es de {PlayerHeight}")
+if PlayerHeight< 1.60:
+    print("Eres Base")
+elif PlayerHeight<1.79:
+    print("Eres Escolta")
+elif PlayerHeight<1.99:
+    print("Eres Alero")
+else:
+    print("Eres Ala-Pivot o Pivot")
