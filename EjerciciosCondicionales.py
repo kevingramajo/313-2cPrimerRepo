76ed070afd8af322bc934ebb67b9ef737d14f9f6
Kevin Gramajo <kevinalexisgramajo@gmail.com>
2025-08-26 17:29:48 -0300

    3Save

--- EjerciciosCondicionales.py
+++ EjerciciosCondicionales.py
@@ -9,12 +9,40 @@
 #else:
     #print("Eres Ala-Pivot o Pivot")
 
-notaParcial = int(input("Colocar Nota: "))
-if notaParcial <=3:
-    print(f"Desaprobado, tu nota es: {notaParcial}")
-elif notaParcial <=5:
-    print(f"Aprobado, tu nota es: {notaParcial}")
-elif notaParcial <=10:
-    print(f"Promocion, tu nota es: {notaParcial}")
-else:
-    print(f"Numero Incorrecto")
+#notaParcial = int(input("Colocar Nota: "))
+#if notaParcial <=3:
+    #print(f"Desaprobado, tu nota es: {notaParcial}")
+#elif notaParcial <=5:
+    #print(f"Aprobado, tu nota es: {notaParcial}")
+#elif notaParcial <=10:
+    #print(f"Promocion, tu nota es: {notaParcial}")
+#else:
+    #print(f"Numero Incorrecto")
+
+#Ejercicio Match
+#destino=str(input("Indique su Lugar de Destino: "))
+#estacion=str(input("Ahora Indique la estacion del Ano (Inviero,Verano,Otono o Primavera) "))
+#match estacion:
+    #case "Invierno":
+        #if destino == "Bariloche":
+            #print("Se Viaja")
+        #else:
+            #print("No Se Viaja")
+    #case "Verano":
+        #if destino == "Mar del Plata":
+            #print("Se Viaja")
+        #elif destino == "Cataratas":
+            #print("Se viaja")
+        #else:
+            #print("No se viaja")
+    #case "Primavera":
+        #if destino == "Bariloche":
+            #print("No se viaja")
+        #else:
+            #print("Se viaje")
+    #case _:
+        #print("Se Viaja")
+print("Bienvenido a Gotita S.A.")
+print("===================================================================")
+str(input("Que tipo de Cliente sos: (Residencial, Comercial o Industrial) "))
+int(input("Ingresar Cantidad de mts³ consumidos: "))
