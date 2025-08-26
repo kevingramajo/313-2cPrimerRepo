0125bd12a331bba331b85cf1af805eceffefc7ad
Kevin Gramajo <kevinalexisgramajo@gmail.com>
2025-08-25 23:48:45 -0300

    2Save

--- EjerciciosCondicionales.py
+++ EjerciciosCondicionales.py
@@ -1,10 +1,20 @@
-PlayerHeight = float(input("Ingresar Altura: "))
-print(f"Tu altura es de {PlayerHeight}")
-if PlayerHeight< 1.60:
-    print("Eres Base")
-elif PlayerHeight<1.79:
-    print("Eres Escolta")
-elif PlayerHeight<1.99:
-    print("Eres Alero")
+#PlayerHeight = float(input("Ingresar Altura: "))
+#print(f"Tu altura es de {PlayerHeight}")
+#if PlayerHeight< 1.60:
+    #print("Eres Base")
+#elif PlayerHeight<1.79:
+    #print("Eres Escolta")
+#elif PlayerHeight<1.99:
+    #print("Eres Alero")
+#else:
+    #print("Eres Ala-Pivot o Pivot")
+
+notaParcial = int(input("Colocar Nota: "))
+if notaParcial <=3:
+    print(f"Desaprobado, tu nota es: {notaParcial}")
+elif notaParcial <=5:
+    print(f"Aprobado, tu nota es: {notaParcial}")
+elif notaParcial <=10:
+    print(f"Promocion, tu nota es: {notaParcial}")
 else:
-    print("Eres Ala-Pivot o Pivot")
+    print(f"Numero Incorrecto")
