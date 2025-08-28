a6b3aa491ba2c4d2bed2b34eb512c763cc98e9fb
Kevin Gramajo <kevinalexisgramajo@gmail.com>
2025-08-28 16:45:46 -0300

    5Save

---
+++ Pruebas/pareimpar.py
@@ -0,0 +1,7 @@
+print("============================================================================")
+print("Numero Par e Impar")
+numeroSelecc=int(input("Seleccionar un Numero: "))
+if numeroSelecc % 2 == 0:
+    print("Es Par")
+else:
+    print("Es Impar")
